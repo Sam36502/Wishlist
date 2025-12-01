af9cc0cced7e37011217fe13a93d2720b58f83d6
Samuel Pearce <sam@aepearce.com>
2025-12-01 17:36:14 +0100

    Updated database initialisation script

--- data/db_schema.sql
+++ data/db_schema.sql
@@ -1,30 +1,23 @@
--- DATABASE STRUCTURE SCRIPT
-
---DROP DATABASE IF EXISTS "wishlist";
---CREATE DATABASE "wishlist" CHARACTER SET utf8;
-
---GRANT INSERT, SELECT, UPDATE, DELETE ON "wishlist".* TO "wishlist_user";
---USE "wishlist";
-
-DROP TABLE IF EXISTS "tbl_user";
-CREATE TABLE "tbl_user" (
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE META_INFO (FIELD TEXT PRIMARY KEY, VALUE TEXT);
+INSERT INTO META_INFO VALUES('schema_version','1.1.0');
+INSERT INTO META_INFO VALUES('created_date','2025-11-11');
+CREATE TABLE IF NOT EXISTS "tbl_user" (
   "id_user" INTEGER NOT NULL PRIMARY KEY,
   "email" TEXT NOT NULL UNIQUE,
   "password" TEXT NOT NULL,
   "name" TEXT NOT NULL
 );
-
-DROP TABLE IF EXISTS "tbl_status";
-CREATE TABLE "tbl_status" (
+CREATE TABLE IF NOT EXISTS "tbl_status" (
   "id_status" INTEGER NOT NULL PRIMARY KEY,
   "name" TEXT NOT NULL,
   "desc" text
 );
-
-INSERT INTO "tbl_status" VALUES (1,'Available','No one is planning to get this, yet.'),(2,'Reserved','Someone is planning to get this already.'),(3,'Received','This was on the wishlist, but has since been received.');
-
-DROP TABLE IF EXISTS "tbl_item";
-CREATE TABLE "tbl_item" (
+INSERT INTO tbl_status VALUES(1,'Available','No one is planning to get this yet.');
+INSERT INTO tbl_status VALUES(2,'Reserved','Someone is planning to get this already.');
+INSERT INTO tbl_status VALUES(3,'Received','This was on the wishlist, but has since been received.');
+CREATE TABLE IF NOT EXISTS "tbl_item" (
   "id_item" INTEGER NOT NULL PRIMARY KEY,
   "name" TEXT NOT NULL,
   "desc" TEXT NOT NULL,
@@ -33,11 +26,10 @@ CREATE TABLE "tbl_item" (
   "reserved_by_user_id" INTEGER DEFAULT NULL REFERENCES "tbl_user" ("user_id") ON DELETE CASCADE ON UPDATE CASCADE,
   "price" INTEGER NOT NULL DEFAULT '0'
 );
-
-DROP TABLE IF EXISTS "tbl_link";
-CREATE TABLE "tbl_link" (
+CREATE TABLE IF NOT EXISTS "tbl_link" (
   "id_link" INTEGER NOT NULL PRIMARY KEY,
   "text" TEXT NOT NULL,
   "hyperlink" TEXT NOT NULL,
   "item_id" INTEGER NOT NULL REFERENCES "tbl_item" ("id_item") ON DELETE CASCADE ON UPDATE CASCADE
 );
+COMMIT;
